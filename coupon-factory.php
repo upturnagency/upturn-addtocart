05b284cc4a1d38e2a92594ce36d0c021c6dd7052
Hakon Schutt <hakon@schutt.no>
2017-11-09 21:58:07 +0100

    added bubble sort for coupons

--- coupon-factory.php
+++ coupon-factory.php
@@ -5,17 +5,30 @@
   require 'price-supports/Price.php';
   require 'price-supports/Product.php';
 
+  function bubble_sort_coupons(array $array){
+  	do {
+  		$swapped = false;
+  		for( $i = 0; $i < count($array) - 1; $i++) {
+  			if($array[$i]->getCondition() > $array[$i + 1]->getCondition()) {
+  				list($array[$i + 1], $array[$i]) = array($array[$i], $array[$i + 1]);
+  				$swapped = true;
+  			}
+  		}
+  	} while($swapped);
+  	return $array;
+  }
+
   $price = new Price(23);
   $product = new Product(20);
   $product2 = new Product(10);
 
-  $items = array($price, $product);
+  $items = array($product, $price);
 
   $items[] = $product2;
 
-  
+  $sorted = bubble_sort_coupons($items);
 
-  foreach($items as $item){
+  foreach($sorted as $item){
     echo $item->rendurHTML();
   }
 ?>
