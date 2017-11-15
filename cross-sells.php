2565d5f0c06ad399851b6878a6a59f1e65eeb33c
Thomas Audunhus <thomas@drivtrafikk.no>
2017-11-15 03:14:49 +0100

    added initial styling of new top

--- cross-sells.php
+++ cross-sells.php
@@ -70,121 +70,109 @@
 
 			    $i = 0;
 			    ?>
-                <div class="added-to-cart">
+                <div class="added-to-cart clearfix">
 				    <?php
 				    while ( $loop->have_posts() && $i < 1) : $loop->the_post(); global $product;
 					    $i++;
 					    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $loop->post->ID ), 'thumbnail' );?>
-                    <i class="check"></i><h3><a href="<?php the_permalink(); ?>"><?php echo $loop->post->post_title;?></a> <?php echo __('added to cart', 'cross-sell'); ?></h3>
+                        <div class="container">
+                            <div class="content">
+                                <i class="check"></i>
+                                <span><?php echo sprintf ( _n( '%d item added to cart', '%d items added to cart', $i ), $i); ?> </span>
+                            </div>
+                        </div>
+                        <div class="container">
+                            <div class="content">
+                                <img src="<?php  echo $image[0]; ?>" data-id="<?php echo $loop->post->ID; ?>" class="product-image">
+                            </div>
+                        </div>
+                        <div class="container">
+                            <div class="content">
+                            <strong><?php echo $loop->post->post_title;?></strong></span>
+                            <?php if(!empty($expire_time)):
+                                echo '<br/><span id="cart-countdown">' . __("We'll reserve it for ", "cross-sell"); ?>
+                                <span id="countdown"><?php echo __('checking...', 'cross-sell'); ?></span>
+                            <?php endif; ?>
+                            </div>
 				    <?php endwhile; ?>
+                        </div>
                 </div>
-                <div class="cart clearfix">
-		            <?php
-		            echo '<strong>' . __('Cart total:', 'cross-sell') . '</strong>&nbsp;' . $woocommerce->cart->get_cart_total();
-		            echo '&nbsp;('.sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ).')';
-		            do_action('before_cross_sell_actions');
-		            ?>
                 </div>
-            </div>
             <?php
             $class = ( get_option('upturn-useStickyHeader') != 'no' ) ? 'sticky-header' : '';
             ?>
             <div class="cross-sell-header bottom <?php echo $class; ?> clearfix cf">
-			    <?php
-
-			    $args = array(
-				    'p'         => $id,
-				    'post_type' => 'product',
-			    );
-			    $loop = new WP_Query( $args );
-
-			    $i = 0;
-			    ?>
-                <div class="item-info">
-				    <?php
-				    while ( $loop->have_posts() && $i < 1) : $loop->the_post(); global $product;
-
-					    $i++;
-					    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $loop->post->ID ), 'thumbnail' );?>
-                        <img src="<?php  echo $image[0]; ?>" data-id="<?php echo $loop->post->ID; ?>" class="product-image">
-                        <div class="item-info--text">
-                            <span class="product clearfix"><strong><?php echo $loop->post->post_title;?></strong>
-                                <?php
-                                if(!empty($expire_time)):
-                                    echo '<br/><span id="cart-countdown">' . __("We'll reserve it for ", "cross-sell"); ?>
-                                    <span id="countdown"><?php echo __('checking...', 'cross-sell'); ?></span></span>
-                                <?php
-                                endif;
-                                ?>
-                            </span>
-                            </span>
-                        </div>
-				    <?php endwhile; ?>
-
-
-
+                <div class="container">
+                    <div class="content">
+                        <?php if ( get_option( 'upturn-displayGoToShopButton' ) == 'yes' ) : ?>
+                            <a href="<?php echo get_category_link( $catid ); ?>" class="button btn black alt gotoshop"><?php echo __('Go to shop', 'cross-sell'); ?></a>
+                        <?php endif; ?>
+                    </div>
                 </div>
-                <div class="actions clearfix">
-                    <?php
-                    // Get Free Shipping Methods for Rest of the World Zone & populate array $min_amounts
+                <div class="container">
+                    <div class="content">
+	                <?php echo '<strong>' . __('Cart total:', 'cross-sell') . '</strong>&nbsp;' . $woocommerce->cart->get_cart_total();
+	                echo '&nbsp;('.sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ).')';
+                    echo '<br>';
+	                do_action('before_cross_sell_actions');
+	                ?>
+	                <?php
+	                // Get Free Shipping Methods for Rest of the World Zone & populate array $min_amounts
 
-                    $default_zone = new WC_Shipping_Zone(0);
-                    $default_methods = $default_zone->get_shipping_methods();
+	                $default_zone = new WC_Shipping_Zone(0);
+	                $default_methods = $default_zone->get_shipping_methods();
 
-                    foreach( $default_methods as $key => $value ) {
-	                    if ( $value->id === "free_shipping" ) {
-		                    if ( $value->min_amount > 0 ) $min_amounts[] = $value->min_amount;
-	                    }
-                    }
+	                foreach( $default_methods as $key => $value ) {
+		                if ( $value->id === "free_shipping" ) {
+			                if ( $value->min_amount > 0 ) $min_amounts[] = $value->min_amount;
+		                }
+	                }
+	                // Get Free Shipping Methods for all other ZONES & populate array $min_amounts
 
-                    // Get Free Shipping Methods for all other ZONES & populate array $min_amounts
+	                $delivery_zones = WC_Shipping_Zones::get_zones();
 
-                    $delivery_zones = WC_Shipping_Zones::get_zones();
+	                foreach ( $delivery_zones as $key => $delivery_zone ) {
+		                foreach ( $delivery_zone['shipping_methods'] as $key => $value ) {
+			                if ( $value->id === "free_shipping" ) {
+				                if ( $value->min_amount > 0 ) $min_amounts[] = $value->min_amount;
+			                }
+		                }
+	                }
 
-                    foreach ( $delivery_zones as $key => $delivery_zone ) {
-	                    foreach ( $delivery_zone['shipping_methods'] as $key => $value ) {
-		                    if ( $value->id === "free_shipping" ) {
-			                    if ( $value->min_amount > 0 ) $min_amounts[] = $value->min_amount;
-		                    }
-	                    }
-                    }
+	                // Find lowest min_amount
 
-                    // Find lowest min_amount
+	                if ( is_array($min_amounts) ) {
 
-                    if ( is_array($min_amounts) ) {
+		                $min_amount = min($min_amounts);
 
-	                    $min_amount = min($min_amounts);
+		                // Get Cart Subtotal inc. Tax excl. Shipping
 
-                        // Get Cart Subtotal inc. Tax excl. Shipping
+		                $current = WC()->cart->subtotal;
 
-	                    $current = WC()->cart->subtotal;
+		                // If Subtotal < Min Amount Echo Notice
+		                // and add "Continue Shopping" button
 
-                        // If Subtotal < Min Amount Echo Notice
-                        // and add "Continue Shopping" button
+		                if ( $current < $min_amount ) {
+			                $added_text = esc_html__('Get free shipping if you order ', 'woocommerce' ) . wc_price( $min_amount - $current ) . esc_html__(' more!', 'woocommerce' );
 
-	                    if ( $current < $min_amount ) {
-		                    $added_text = esc_html__('Get free shipping if you order ', 'woocommerce' ) . wc_price( $min_amount - $current ) . esc_html__(' more!', 'woocommerce' );
+		                }else{
+			                $added_text = esc_html__('You get free shipping on your order! ', 'woocommerce' );
+		                }
+		                print_r($added_text);
+	                }
 
-	                    }else{
-		                    $added_text = esc_html__('You get free shipping on your order! ', 'woocommerce' );
-                        }
-	                    print_r($added_text);
-                    }
-
-                    ?>
-                    <div class="buttons">
-                      <?php
-                      if ( get_option( 'upturn-displayGoToCartButton' ) == 'yes' ) : ?>
+	                ?>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="content">
+	                <?php if ( get_option( 'upturn-displayGoToCartButton' ) == 'yes' ) : ?>
                         <a href="<?php echo $woocommerce->cart->get_cart_url(); ?>" class="button btn cart"><?php echo __('Go to cart', 'cross-sell'); ?></a>
-                      <?php endif;
+	                <?php endif; ?>
 
-                      if ( get_option( 'upturn-displayCheckoutButton' ) == 'yes' ) : ?>
+	                <?php if ( get_option( 'upturn-displayCheckoutButton' ) == 'yes' ) : ?>
                         <a href="<?php echo $woocommerce->cart->get_checkout_url(); ?>" class="button btn alt checkout"><?php echo __('Checkout', 'cross-sell'); ?></a>
-                      <?php endif;
-
-                      if ( get_option( 'upturn-displayGoToShopButton' ) == 'yes' ) : ?>
-                        <a href="<?php echo get_category_link( $catid ); ?>" class="button btn black alt gotoshop"><?php echo __('Go to shop', 'cross-sell'); ?></a>
-                      <?php endif; ?>
+	                <?php endif; ?>
                     </div>
                 </div>
             </div>
